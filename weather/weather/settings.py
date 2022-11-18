a2b2e6077c5a9c79a551868d351922f7178ada2d
Aleksey <Deep1985@yandex.ru>
2022-11-18 21:40:39 +0300

    index.html

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'weather.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
