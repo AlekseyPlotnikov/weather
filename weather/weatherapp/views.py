e82258acf4acd132c3857d06c2b60e5e094602bb
Aleksey <Deep1985@yandex.ru>
2022-11-19 22:24:42 +0300

    index.html фото погоды

--- weather/weatherapp/views.py
+++ weather/weatherapp/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+import requests
 
 
 def index(request):
-    return render(request, 'weather/index.html')
+    appid = '5ad1dfc918bfd06bd401fa4b21db0748'
+    url = ' https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+    city = 'London'
+    res = requests.get(url.format(city)).json()
+    city_ifo = {
+        'city': city,
+        'temp': res['main']['temp'],
+        'icon': res['weather'][0]['icon']
+    }
+    context = {'info': city_ifo}
+    return render(request, 'weather/index.html', context)
